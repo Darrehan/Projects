8ae46c7273598a67fc9d3dc8376bed0821fce0b1
Darrehan <darrehanrasool@gmail.com>
2024-09-16 13:38:45 +0530

    Dar Rehan Rasool
    sycomusiclover

---
+++ IOS Projects/Application-1/Application-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Application-1
+//
+//  Created by Rehan Rasool on 16/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Rehan Rasool!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
